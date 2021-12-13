7253483e59bbc53a2d923b0e2152f93661c08165
Marcolux <mccolucci1@gmail.com>
2021-12-13 13:19:45 -0600

    k

--- plant_shop/src/pages/Checkout.js
+++ plant_shop/src/pages/Checkout.js
@@ -5,13 +5,9 @@ import axios from "axios"
 
 const Checkout = (props)=>{
 
-
-    // let orderDate = new Date(year, monthIndex, day)
-    
-    const [date, setDate] = useState(new Date())
     const [shipping_address, setShippingAdd] = useState('')
     const [credit_card_number, setCardNumber] = useState(0)
-    const [total, setTot] = useState(props.total_price)
+    const [total, setTot] = useState(0)
     const [plantOrder, setPlantOrder] = useState([])
     
     
@@ -28,13 +24,9 @@ const Checkout = (props)=>{
         })
         setPlantOrder([...plantOrder])
       
-        await axios.post(`http://localhost:3001/order/${props.user.id}`, {date, shipping_address,credit_card_number,total, plantOrder}  )
+        await axios.post(`http://localhost:3001/order/${props.user.id}`, {shipping_address,credit_card_number, total , plantOrder}  )
         
-        props.setCartList([])
-        setDate('')
-        setShippingAdd('')
-        setCardNumber(0)
-        props.setTotal(0)
+       
         
     }
 
@@ -46,16 +38,8 @@ const Checkout = (props)=>{
         <div className="Checkout"> 
         <h1>Checkout Page</h1>
         <form className="CheckoutForm" onSubmit={createOrder}>
-            <div>
-              <label>Date</label>
-            <input
-             value={date}
-             onChange={(e)=>{setShippingAdd(e.target.value)}
-            }
-             />
-           </div>
             <div >
-            <label>Shipping Address</label>
+                <label>Shipping Address</label>
             <input
              value={shipping_address}
              onChange={(e)=>{setShippingAdd(e.target.value)}
@@ -70,30 +54,31 @@ const Checkout = (props)=>{
             }
               />
               </div>
+              <div>
+            <label>Total Price</label>
+            <input
+             value={props.total_price}
+             onChange={(e)=>{props.setTotal(e.target.value)
+               setTot(props.total_price)
+            }
+            }
+             />
+             </div>
              <div className="itemCheckout">
                  {
                      props.cartList.map((item,i)=>{
                          
                          return(
-                             <div  key={i}>
+                         <div  key={i}>
                          <p>{item.name}</p>
                          <p>{item.price}</p>
                         
                          </div>
                          )
-                        })
-                    }
+                     })
+                 }
              </div>
-            <label>Total Price</label>
-            <input
-                value={props.total_price}
-                onChange={(e)=>{props.setTot(e.target.value)
-                setTot(props.total_price)
-            
-            }
-            }
-                />
-                <input type='submit' onClick={()=> {}}value='checkout'/>
+                <input type='submit' value='checkout'/>
             <Link to="/orders"><p>checkout</p></Link>
             
         </form>
